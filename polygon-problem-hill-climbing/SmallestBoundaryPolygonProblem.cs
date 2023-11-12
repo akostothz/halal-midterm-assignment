fe2addaaab90224e78e1b1cb38259a76dd80374f
akostothz <akostothza@gmail.com>
2023-11-12 23:13:46 +0100

    Small changes

--- polygon-problem-hill-climbing/SmallestBoundaryPolygonProblem.cs
+++ polygon-problem-hill-climbing/SmallestBoundaryPolygonProblem.cs
@@ -8,10 +8,11 @@ namespace polygon_problem_hill_climbing
 {
     public class SmallestBoundaryPolygonProblem
     {
-
+        List<Point> Q;
+        List<Point> P;
         List<Point> Points;
-        double fitness;
-        double prev_fitness;
+        double p_fitness;
+        double last_fitness;
         double errorMargin;
         double t;
         double epsilon;
@@ -19,16 +20,19 @@ namespace polygon_problem_hill_climbing
         public SmallestBoundaryPolygonProblem(double epsilon, double t, double errorMargin)
         {
             Points = new List<Point>();
+            Q = new List<Point>();
+            P = new List<Point>();
             this.epsilon = epsilon;
             this.t = t;
             this.errorMargin = errorMargin;
-            fitness = double.MaxValue;
-            prev_fitness = double.MaxValue;
+            p_fitness = double.MaxValue;
+            last_fitness = double.MaxValue;
         }
 
-        public void Run(string filename)
+        public void RunHC_Stachostic(string filename)
         {
             LoadPointsFromFile(filename);
+            Optimize();
         }
 
         void LoadPointsFromFile(string filename)
@@ -41,6 +45,20 @@ namespace polygon_problem_hill_climbing
 
         }
 
+        void Optimize()
+        {
+
+        }
+
+        bool StopCondition()
+        {
+            if ((last_fitness - p_fitness) / last_fitness <= errorMargin)
+            {
+                t--;
+            }
+            return (t <= 0);
+        }
+
         double DistanceFromLine(Point lp1, Point lp2, Point p)
         {
             // https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
@@ -49,14 +67,57 @@ namespace polygon_problem_hill_climbing
 
         double OuterDistanceToBoundary(List<Point> solution)
         {
-            return 0;
+            double sum_min_distances = 0;
+
+            for (int pi = 0; pi < Points.Count; pi++)
+            {
+                double min_dist = double.MaxValue;
+                for (int li = 0; li < solution.Count; li++)
+                {
+                    double act_dist = DistanceFromLine(solution[li], solution[(li + 1) % solution.Count], Points[pi]);
+                    if (li == 0 || act_dist < min_dist)
+                        min_dist = act_dist;
+                }
+                if (min_dist < 0)
+                    sum_min_distances += -min_dist;
+            }
+            return sum_min_distances;
         }
 
         double LengthOfBoundary(List<Point> solution)
         {
-            return 0;
+            double sum_length = 0;
+
+            for (int li = 0; li < solution.Count - 1; li++)
+            {
+                Point p1 = solution[li];
+                Point p2 = solution[(li + 1) % solution.Count];
+                sum_length += Math.Sqrt(Math.Pow(p1.x - p2.x, 2) + Math.Pow(p1.y - p2.y, 2));
+            }
+            return sum_length;
         }
 
+        double Objective(List<Point> solution)
+        {
+            return LengthOfBoundary(solution);
+        }
+
+        double Constraint(List<Point> solution)
+        {
+            return -OuterDistanceToBoundary(solution);
+        }
+
+    }
+
+    public static class RNG
+    {
+        static Random rnd;
+
+        public static double GenerateRandomDoubleWithBounds(double lowerBound, double upperBound)
+        {
+            rnd = new Random();
+            return rnd.NextDouble() * (upperBound - lowerBound) + lowerBound;
+        }
     }
 
     public class Point
